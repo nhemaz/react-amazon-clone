5ba712572cc45768f73afcd2a0ca3b1af31d96a7
cliff.io <cliffnhemaz@gmail.com>
2021-06-26 19:08:08 +0200

    Place order action

--- backend/utils.js
+++ backend/utils.js
@@ -18,17 +18,20 @@ export const generateToken = (user) => {
 export const isAuth = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (authorization) {
-    const token = authorization.slice(7, authorization.length); // Bearer XXXXX
-    jwt.verify(token, process.env.JWT_SECRET || '33344433433', (err, decode) => {
-     if(err) {
-       res.status(401).send({ message: 'Invalid Token' });
-     }else {
-       res.user = decode;
-       next();
-     }
-    })
+    const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET || '33344433433',
+      (err, decode) => {
+        if (err) {
+          res.status(401).send({ message: 'Invalid Token' });
+        } else {
+          req.user = decode;
+          next();
+        }
+      }
+    );
   } else {
-        res.status(401).send({ message: 'No Token' });
-
+    res.status(401).send({ message: 'No Token' });
   }
-}
+};
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,73 +1,72 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import { signout } from './actions/userActions';
-import CartScreen from './screens/cartScreen';
+import cartScreen from './screens/cartScreen';
 import HomeScreen from './screens/HomeScreen';
+import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import ProductScreen from './screens/ProductScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SigninScreen from './screens/SigninScreen';
-import ShippingAddressScreen from './screens/ShippingAddressScreen'
-import PaymentMethodScreen from './screens/PaymentMethodScreen'
-import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 function App() {
-
-const cart = useSelector(state => state.cart);
+  const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
   const dispatch = useDispatch();
   const signoutHandler = () => {
     dispatch(signout());
-  }
+  };
   return (
     <BrowserRouter>
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <Link className="brand" to="/">
-            Fiege
-          </Link>
-        </div>
-        <div>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link className="brand" to="/">
+              fiege
+            </Link>
+          </div>
+          <div>
             <Link to="/cart">
               Cart
-          {cartItems.length > 0 && (
-            <span className="badge">{cartItems.length}</span>
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
               )}
             </Link>
-            {
-              userInfo ? (
-                <div className="dropdown">
-                  <Link to="#">Hi, {userInfo.name} <i className="fa fa-caret-down"></i></Link>
-                  <ul className="dropdown-content">
-                    <Link to="/#signout" onClick={signoutHandler}>Sign Out</Link>
-                  </ul>
-                </div>
-              ) :
-                (
-                  <Link to="/signin">Sign In</Link>
-                )
-            }
-
-        </div>
-      </header>
-      <main>
-       <Route path="/cart/:id?" component={CartScreen}></Route>
-       <Route path="/product/:id" component={ProductScreen}></Route>
-       <Route path="/signin" component={SigninScreen}></Route>
-       <Route path="/register" component={RegisterScreen}></Route>
-       <Route path="/shipping" component={ShippingAddressScreen}></Route>
-       <Route path="/payment" component={PaymentMethodScreen}></Route>
-       <Route path="/placeorder" component={PlaceOrderScreen}></Route>
-       <Route path="/" component={HomeScreen} exact></Route>
-
-      </main>
-      <footer className="row center">All Rights Reserved.</footer>
-    </div>
-  </BrowserRouter>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="#signout" onClick={signoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
+          </div>
+        </header>
+        <main>
+          <Route path="/cart/:id?" component={cartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
+          <Route path="/shipping" component={ShippingAddressScreen}></Route>
+          <Route path="/payment" component={PaymentMethodScreen}></Route>
+          <Route path="/placeorder" component={PlaceOrderScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">All Rights Reserved.</footer>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
