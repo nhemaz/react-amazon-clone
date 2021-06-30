08885804616694f312692be835f08fd97cdd705c
cliff.io <cliffnhemaz@gmail.com>
2021-06-30 13:18:39 +0200

    Order model fix

--- routers/orderRouter.js
+++ routers/orderRouter.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
-import Order from '../models/orderModel.js';
+import Order from '../models/OrderModel';
 import { isAuth, isAdmin } from '../utils.js';
 
 const orderRouter = express.Router();
