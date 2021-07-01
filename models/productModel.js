cd0077ef1f772ca75276b09945354f475214aa63
cliff.io <cliffnhemaz@gmail.com>
2021-07-01 20:42:30 +0200

    models update

--- models/productModel.js
+++ models/productModel.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
 
-const productSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema(
+  {
     name: { type: String, required: true, unique: true },
-    seller: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    seller: { type: mongoose.Schema.Types.ObjectID, ref: 'User' },
     image: { type: String, required: true },
     brand: { type: String, required: true },
     category: { type: String, required: true },
@@ -11,11 +12,11 @@ const productSchema = new mongoose.Schema({
     countInStock: { type: Number, required: true },
     rating: { type: Number, required: true },
     numReviews: { type: Number, required: true },
-}, {
+  },
+  {
     timestamps: true,
-});
-
-
+  }
+);
 const Product = mongoose.model('Product', productSchema);
 
 export default Product;
